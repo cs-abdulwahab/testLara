36659d47a41cc846722ad0006e938d0589973621
Abdul Wahab <cs.abdulwahab@gmail.com>
2016-09-19 19:50:05 +0500

    just a small change

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,1 +1,27 @@
-this is the inedx of employee  view
+@extends('layouts.app')
+
+@section('content')
+
+    <table class="table table-bordered table-hover">
+        <thead>
+        <tr>
+            <th> Employee Name</th>
+            <th> Username</th>
+        </tr>
+        </thead>
+        <tbody>
+
+
+
+        <tr>
+            <td> ds</td>
+            <td> ds</td>
+        </tr>
+
+
+        </tbody>
+    </table>
+
+
+
+@stop
