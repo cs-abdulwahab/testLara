2ea6e454fe45fd5dd85e5a5f1abd51c7ee57781a
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-10-03 09:52:29 +0500

    components slots of lara 5.4 and vue js

---
+++ resources/views/jumbotron.blade.php
@@ -0,0 +1,12 @@
+<div class="jumbotron">
+    <h1 class="display-3">{{$title}}</h1>
+    <p class="lead">{{$slot}}</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+
+
+        <p class="lead">
+            <a class="btn btn-primary btn-lg pull-right" href="#" role="button">{{$button or "Press me"}}</a>
+        </p>
+
+</div>
